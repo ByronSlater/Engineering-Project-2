630d87724c600127ee67a13e83172138cdbc3c93
Michael <mconnolly.se@gmail.com>
2026-09-02 10:42:10 +0100

    Adding likes to post

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 
 import lombok.Data;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 @Entity
 @Table(name = "POSTS")
@@ -13,6 +16,24 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+
+
+    // tells java to use the likes table and to connect
+    // posts and users together
+    @ManyToMany
+    @JoinTable(
+            name = "likes",
+            // column in the likes points to the post
+            joinColumns = @JoinColumn(name = "post_id"),
+            // column in the likes points to the user
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+
+    // stores all users who have liked a post
+    // dont know much about sets but had to use this as i didnt
+    // want user to like the same post twice
+    // the UNIQUE (user_id, post_id) also does this, so technically 2 players of protection
+    private Set<User> likedBy = new HashSet<>();
 
     public Post() {}
 
