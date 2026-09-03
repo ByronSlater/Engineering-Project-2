e3b7a3fee035792d5f5d8db42248cf8177a092b5
charlo-adams <charlotteadams965@gmail.com>
2026-09-03 11:07:15 +0100

    adding individual profiles, more work tbd

--- src/main/java/com/makersacademy/acebook/controller/UsersController.java
+++ src/main/java/com/makersacademy/acebook/controller/UsersController.java
@@ -1,20 +1,32 @@
 package com.makersacademy.acebook.controller;
 
+
+import com.makersacademy.acebook.model.Post;
 import com.makersacademy.acebook.model.User;
+import com.makersacademy.acebook.repository.PostRepository;
 import com.makersacademy.acebook.repository.UserRepository;
+import com.makersacademy.acebook.service.PostService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+
 
 import java.util.Random;
 
 @RestController
 public class UsersController {
     final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final PostService postService;
 
-    UsersController(UserRepository userRepository) {
+    UsersController(UserRepository userRepository, PostRepository postRepository, PostService postService) {
         this.userRepository = userRepository;
+        this.postRepository = postRepository;
+        this.postService = postService;
     }
 
     @GetMapping("/users/after-login")
@@ -50,4 +62,26 @@ public class UsersController {
         Random random = new Random();
         return profiles[random.nextInt(profiles.length)];
     }
+
+    @GetMapping("/profile")
+    public RedirectView profiles(@AuthenticationPrincipal DefaultOidcUser principal){
+        String username = principal.getEmail();
+
+        User user = userRepository.findUserByUsername(username).orElseThrow();
+        RedirectView myredirectView = new RedirectView("/profile/" + user.getId());
+        return myredirectView;
+    }
+
+    @GetMapping("/profile/{id}")
+    public ModelAndView DisplayProfile (@PathVariable Long id){
+        User user = userRepository.findById(id).orElseThrow();
+        var posts = postService.allPosts().stream().filter(post -> post.getUser().getId().equals(user.getId())).toList();
+
+        ModelAndView Profile = new ModelAndView("profile");
+        Profile.addObject("user", user);
+        Profile.addObject("posts", posts);
+        return Profile;
+    }
+
+
 }
