6d7720bc1cac022cffa85c0acc0ec9b4b841b7c1
DaphnaAzari <181016677+DaphnaAzari@users.noreply.github.com>
2026-09-03 13:58:51 +0100

    Test for About Us page

---
+++ src/test/java/com/makersacademy/acebook/feature/AboutUsTest.java
@@ -0,0 +1,55 @@
+package com.makersacademy.acebook.feature;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+//starts real app on an actual port, rather than a simulated in-memory request
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+//tell springboot to start the entire app:
+@ActiveProfiles("test")
+public class AboutUsTest {
+    //this test runs against test db:
+    //driver -automated chrome browser, every action goes through this object:
+    WebDriver driver;
+    //faker generates random realistic fake data so each test runs on fresh data
+    Faker faker;
+
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver();
+        faker = new Faker();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void loggedInUserCanNavigateToAboutUsPage() {
+        String email = faker.name().username() + "@email.com";
+//        sign up, that logs in the user:
+        driver.get("http://localhost:8081/");
+        driver.findElement(By.linkText("Sign up")).click();
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys("P@55qw0rd");
+        //finds the form and types text into it , simulating a real user, then finds the submit and simulates it
+        driver.findElement(By.name("action")).click();
+
+        //navigate to about us, and search for About:
+        driver.findElement(By.linkText("About")).click();
+        //finds first h1 and checks it's equal to About Us:
+        String heading = driver.findElement(By.tagName("h1")).getText();
+        assertEquals("About Us", heading);
+    }
+}
